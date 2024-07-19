9dbf2b46802d4060ae6336c5a83d5b7aed736e99
colbymoyer <colbymoyer@aol.com>
2024-07-19 10:59:54 -0400

    update

---
+++ src/shared/queries/userQuery.js
@@ -0,0 +1,123 @@
+const { bcrypt, client, uuid, jwt, prisma } = require("../shared");
+const JWT = process.env.JWT || "123456";
+
+const registerUser = async ({ password, firstName, lastName, email }) => {
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await prisma.user.create({
+    data: {
+      password: hashPassword,
+      firstName,
+      lastName,
+      email,
+    },
+  });
+
+  const token = await jwt.sign({ id: newUser.id }, JWT, {
+    expiresIn: "1h",
+  });
+  return token;
+};
+
+const logInUser = async ({ email, password }) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  console.log(user);
+  // error handling
+  if (!user) {
+    const error = Error("User not found");
+    error.status = 404;
+    throw error;
+  }
+  // use bcrypt compare for the password, and return users information and json web token
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword) {
+    const error = Error("Password not found");
+    error.status = 401;
+    throw error;
+  }
+
+  const token = await jwt.sign({ id: user.id }, JWT, {
+    expiresIn: "1h",
+  });
+  return token;
+};
+
+const getAllUser = async () => {
+  const users = await prisma.user.findMany();
+  return users;
+};
+
+const findUserByToken = async (header) => {
+  const token = header?.split(" ")[1];
+  let id = "";
+  try {
+    const payload = await jwt.verify(token, JWT);
+    id = payload.id;
+  } catch (ex) {
+    const error = Error("Not Authorized");
+    error.status = 404;
+    throw error;
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return user;
+};
+
+const deleteUserById = async (id) => {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    console.error(`Error deleting user with ${id}: `, error);
+    throw new Error("failed to delete user");
+  }
+};
+
+const updateUserById = async (id, body) => {
+  try {
+    const { firstName, lastName, password, email } = body;
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        password: hashPassword,
+        firstName,
+        lastName,
+        email,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    console.error(`Error updating user with ${id}: `, error);
+    throw new Error("failed to update user");
+  }
+};
+
+module.exports = {
+  registerUser,
+  getAllUser,
+  findUserByToken,
+  prisma,
+  logInUser,
+  deleteUserById,
+  updateUserById,
+};
---
+++ src/shared/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const { routes } = require("../shared");
+const { findUserByToken } = require("../queries/userQuery");
+const { register, displayAll, login, deleteUser, updateUser } = require("../controllers/userControllers");
+
+const isLoggedIn = async (req, res, next) => {
+  try {
+    req.user = await findUserByToken(req.headers.authorization);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+routes.post("/register", register);
+routes.post("/login", login);
+routes.get("/all_users", isLoggedIn, displayAll);
+routes.delete("/:id", isLoggedIn, deleteUser);
+routes.put("/:id", isLoggedIn, updateUser);
+
+module.exports = routes;
